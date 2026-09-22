46e277007419301c11d5750b43333bd51fe973cd
Alessandro Colombo <alessandrocolombo1105@gmail.com>
2026-09-22 10:45:17 +0200

    chore(scaffold): aggiunto componente home generato dalla CLI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('ngdemo');
+  protected readonly title = 'ngdemo';
 }
